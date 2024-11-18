8013d30ab9c85a4b95d633e6e3b435aab88bc6ce
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-11-18 09:42:57 +0200

    Interfaces and services uploaded.

---
+++ Contacts/Contracts/IContactService.cs
@@ -0,0 +1,27 @@
+﻿using Contacts.Models;
+
+namespace Contacts.Contracts
+{
+    public interface IContactService
+    {
+        Task AddContactToTeamAsync(string userId, int id);
+
+        bool CheckDeleteOrEditByIdAsync(int id);
+
+        Task<ContactFormModel> CreateEditContactModelAsync(int id);
+
+        Task CreateNewContactEntityAsync(ContactFormModel model);
+
+        Task DeleteContactAsync(int id);
+
+        Task EditExistingContactAsync(int id, ContactFormModel model);
+
+        Task<IEnumerable<ContactViewModel>> GetAllContactsAsync();
+
+        Task<IEnumerable<ContactViewModel>> GetTeamContactsAsync(string userId);
+
+        Task<bool> IsContactExisting(int id);
+
+        Task RemoveContactFromTeamAsync(string userId, int id);
+    }
+}
