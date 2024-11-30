4193d69075fa06700c7b5980b50c41ff9e82bae3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-11-30 15:17:47 +0200

    Delete Contacts directory

--- Contacts/Data/DataModels/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Contacts.Data.DataModels
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public IEnumerable<ApplicationUserContact> ApplicationUsersContacts { get; set; } = new List<ApplicationUserContact>();
-    }
-}
